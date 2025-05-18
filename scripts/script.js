e50232afc11d2c42910d406fb8a005dea1903145
fishellVvv <victorvigilvela@gmail.com>
2025-05-18 12:33:25 +0200

    svg añadidos y arreglado el funcionamiento del menu (a falta de ordenarlo)

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,16 @@
 document.querySelector(".menu-toggle").addEventListener("click", () => {
   document.querySelector(".nav-list").classList.toggle("open");
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const toggle = document.querySelector(".theme-toggle");
+  const icon = document.getElementById("theme-icon");
+
+  toggle.addEventListener("click", () => {
+    const html = document.documentElement;
+    const isDark = html.getAttribute("data-theme") === "dark";
+
+    html.setAttribute("data-theme", isDark ? "light" : "dark");
+    icon.textContent = isDark ? "dark_mode" : "light_mode";
+  });
+});
